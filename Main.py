6e5e06cc19001d69634955349f1a166f5c7e36de
PhilGanney <philipganney@gmail.com>
2017-02-23 18:43:48 +0000

    Update Main.py
    This edit added a function for making anchor tags - as a slightly more useful example of how using python to generate html could be useful.

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
 #website html is basically just made up of strings, so we can use python variables and the format function to make up html that we could copy out and save as web pages
-htmlP= "<p class=\"{0}\">{1}</p>"
+htmlP = "<p class=\"{0}\">{1}</p>"
 
-
+def makeLink(href, text, target = False):
+    optionalAttributes = ""
+    if target:
+        optionalAttributes += ", target=\"" + target + "\""
+    return "<a href=\"" + href + "\""+ optionalAttributes  + ">" + text + "</a>" #_blank _parent _self _top"
+htmlATest = makeLink("google.com", "click here", "fg")
 htmlTemplate3 = "<!DOCTYPE html>\n<html>\n<head>\n<title>{0}</title>\n</head>\n<body>{1}</body>\n</html>"
 openingParagraph = htmlP.format("content", "Hello and welcome to our awesome website")
-print(htmlTemplate3.format("Python generated Site", openingParagraph))
+#print(htmlTemplate3.format("Python generated Site", openingParagraph))
+print(htmlATest)
