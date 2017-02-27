c1d01dac126691c509e2c7d169b9a232c690970e
Phil Ganney <philipganney@gmail.com>
2017-02-27 17:48:46 +0000

    Update Main.py
    Separated the validation code from makeLink into a wrapper.
    Also:
    Added comment at top that states the two places I publish this work (github included)
    Added some seperator comment lines to clarify where the demo code starts

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
+#code released on github as "python-html" (with a MIT liscence) and sololearn code playground as "(add to me) python writing html", use alter "fork" and save as however you please, but don't claim credit for the stuff I've done
 
 import re #regex comparisons used for checking frame names are valid
 
 #website html is basically just made up of strings, so we can use python variables and the format function to make up html that we could copy out and save as web pages
 htmlP = "<p class=\"{0}\">{1}</p>"
 
-def makeLink(href, text, target = False): #targets: _blank, _parent, _self, _top, framename
-    optionalAttributes = ""
+#does some validation before calling makeLink
+def makeLinkVal(href, text, target = False):
+    
     if target:
         #check that the target is a viable choice: either an exact match in the list of targets or a name of an html frame on the page that it can go to
         regexForFrameNames = r"^[\S]+$" #in html5 only restriction is no whitespace -html4 specified other rules,
@@ -18,11 +20,22 @@ def makeLink(href, text, target = False): #targets: _blank, _parent, _self, _top
             else:
                 print("Frame names don't have whitespace")
                 return #if this line runs, the target was not valid
-        
-        #now its checked, put the target and the attribute "target" into a string for adding into the main anchor tag later
+    return makeLink(href, text, target)
+    
+def makeLink(href, text, target = False): 
+    #print("makeLink called: href {}, text {}, target {}".format(href, text, target))
+    optionalAttributes = ""
+    if target:
+    #targets: _blank, _parent, _self, _top, framename
+        # put the target and the attribute "target" into a string for adding into the main anchor tag
         optionalAttributes += ", target=\"" + target + "\""
     return "<a href=\"" + href + "\""+ optionalAttributes  + ">" + text + "</a>" #_blank _parent _self _top"
-htmlATest = makeLink("google.com", "click here", "Frame1_2")
+    
+####ONLY TEST AND DEMO CODE BELOW THIS   
+##
+##
+##
+htmlATest = makeLinkVal("google.com", "click here", "Frame1_2")
 htmlTemplate3 = "<!DOCTYPE html>\n<html>\n<head>\n<title>{0}</title>\n</head>\n<body>{1}</body>\n</html>"
 openingParagraph = htmlP.format("content", "Hello and welcome to our awesome website")
 #print(htmlTemplate3.format("Python generated Site", openingParagraph))
